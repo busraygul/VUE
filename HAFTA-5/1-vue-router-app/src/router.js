7fa262f171f60f44c877ed13ac11164cc7dd77d1
busraygul <aygul.busra58@gmail.com>
2025-09-09 17:26:31 +0300

    vue-router-app

--- HAFTA-5/1-vue-router-app/src/router.js
+++ HAFTA-5/1-vue-router-app/src/router.js
@@ -1,5 +1,18 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
+const routes = [
+    {
+        name: "HomePage",
+        path: "/",
+        component: () => import("@/views/HomePage.vue")
+    },
+    {
+        name: "NewBookmark",
+        path: "/newbookmark",
+        component: () => import("@/views/NewBookmark.vue")
+    },
+]
+
 const router = createRouter({
     routes,
     history: createWebHashHistory()
