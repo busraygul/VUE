30f69c513bd93d2db40b8ed22daa13e6d90fed11
busraygul <aygul.busra58@gmail.com>
2025-09-11 17:12:19 +0300

    ders-6 son

--- ders-6/socialmark-uygulamasi/src/views/LoginPage.vue
+++ ders-6/socialmark-uygulamasi/src/views/LoginPage.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="login_register_container">
     <h3 class="text-2xl text-center mb-3">Login</h3>
-    <input @click="onSubmit" v-model="userData.username" type="text" placeholder="Kullanıcı Adı" class="input mb-3" />
-    <input @click="onSubmit" v-model="userData.password" type="password" placeholder="Şifre" class="input mb-3" />
-    <button class="default-button">Giriş yap</button>
+    <input v-model="userData.username" type="text" placeholder="Kullanıcı Adı" class="input mb-3" />
+    <input v-model="userData.password" type="password" placeholder="Şifre" class="input mb-3" />
+    <button @click="onSubmit" class="default-button">Giriş yap</button>
     <span class="text-center mt-3 text-sm">
       Üye değilim,
       <router-link :to="{ name: 'RegisterPage' }" class="text-red-900 hover:text-black">
@@ -27,10 +27,16 @@ export default {
   }, methods: {
     onSubmit() {
       // GET /posts?title=json-server&author=typicode
-      const password = CryptoJS.AES.encrypt(this.userData.password, this.$store.getters._saltKey).toString();
+      const password = CryptoJS.HmacSHA1(this.userData.password, this.$store.getters._saltKey).toString();
+      this.$appAxios.get(`/users?username=${this.userData.username}&password=${password}`).then(login_response => {
+        if(login_response?.data?.length > 0){
+        this.$store.commit("setUser", login_response ?.data[0]);
+        this.$router.push({name : "HomePage"});
 
-      this.$appAxios.get(`/users?username= ${this.userData.username}&password=${password}`).then(login_response => {
-        console.log("login_response :>> ", login_response);
+        }else{
+          alert("Böyle Bir Kullanıcı Bulunamadı!")
+        }
+        console.log(login_response);
       })
       .catch(e=> console.log(e))
       // .finally(() => this.loader = false);
