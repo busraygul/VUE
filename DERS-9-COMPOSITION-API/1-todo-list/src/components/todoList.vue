a6d9644fc0d7e43f010630b32de2345357920264
busraygul <aygul.busra58@gmail.com>
2025-09-17 16:15:55 +0300

    composition-api-todo-list

---
+++ DERS-9-COMPOSITION-API/1-todo-list/src/components/todoList.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="mt-4">
+        <TodoListItem @delete-event="$emit('delete-event', $event)" v-for="todoItem in todoList" :key="todoItem.id" :todo="todoItem" />
+        <small>{{todoLength}} Adet Item Vardır.</small>
+        <SummaryPage :todoList="todoList" />
+    </section>
+</template>
+
+<script>
+import TodoListItem from './todoListItem.vue';
+import SummaryPage from './SummaryPage.vue';
+import {computed} from "vue";
+export default {
+    components: { 
+        TodoListItem, 
+        SummaryPage 
+    },
+    props: {
+        todoList: Array,
+    },
+    setup(props){
+        const todoLength = computed(() => props.todoList.length)
+        return { todoLength }
+    }
+};
+</script>
