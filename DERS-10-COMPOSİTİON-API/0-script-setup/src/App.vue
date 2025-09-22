0273476b712ece5e495b69f781f10a90e5d7dc15
busraygul <aygul.busra58@gmail.com>
2025-09-22 16:13:07 +0300

    proje

--- DERS-10-COMPOSİTİON-API/0-script-setup/src/App.vue
+++ DERS-10-COMPOSİTİON-API/0-script-setup/src/App.vue
@@ -4,11 +4,36 @@
   <button @click="inc"> {{ counter }}</button>
   <hr>
   <oddOrEven :counter="counter" @odd-event="alertMe" />
+  <hr>
+  <h1>User App</h1>
+  <input type="text" v-model="state.personal.name">
+  <input type="text" v-model="state.personal.lname">
+  {{ state.personal }} 
 </template>
-
 <script setup>
+// beforeCreate() created() ve data() yerine setup kullanılıyor
+import { reactive, ref } from "vue";
 import oddOrEven from "./components/oddOrEven.vue";
 import Utils from "./composable/Utils";
-
+import { watch } from "vue";
 const { title, counter, inc, alertMe } = Utils();
+
+//!UserApp
+const state = reactive({
+  personal: {
+    name: "Büşra",
+    lname: "Ay",
+  },
+  itemList: []
+});
+//const itemList = ref([]);
+
+watch(
+  () => JSON.parse(JSON.stringify(state.personal)),
+  (newPersonal, oldPersonal) => {
+    console.log(oldPersonal);
+    console.log(newPersonal);
+  }
+);
+console.log("state :>> ", state.personal.name);
 </script>
