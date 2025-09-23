399684601cf8747e5daedec7d49140e9fb08f4b9
busraygul <aygul.busra58@gmail.com>
2025-09-23 11:19:03 +0300

    suspense ve teleport example

--- DERS-11/suspense-example/src/App.vue
+++ DERS-11/suspense-example/src/App.vue
@@ -1,8 +1,35 @@
+<script setup>
+import { onErrorCaptured, ref, defineAsyncComponent } from "@vue/runtime-core";
+// import todosPage from "./components/todosPage.vue";
+// import usersPage from "./components/usersPage.vue";
+const err = ref(null);
+const todosPage = defineAsyncComponent(() => import("./components/todosPage.vue"));
+const usersPage = defineAsyncComponent(() => import("./components/usersPage.vue"));
+onErrorCaptured((e) => {
+  err.value = e;
+  return true;
+});
+</script>
+
 <template>
-<h1>App</h1>
-<todosPage />
+  <span v-if="err" class="error"> {{ err }}</span>
+  <suspense v-else>
+    <template #default>
+      <div>
+        <usersPage />
+        <hr />
+        <todosPage />
+      </div>
+    </template>
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </suspense>
 </template>
 
-<script setup>
-import todosPage from './components/todosPage.vue';
-</script>
+<style scoped>
+.error {
+  color: red;
+  font-size: 20px;
+}
+</style>
--- DERS-11/suspense-example/src/components/todosPage.vue
+++ DERS-11/suspense-example/src/components/todosPage.vue
@@ -2,16 +2,23 @@
 import { ref } from "vue";
 
 const todoList = ref([]);
-fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then((json) =>(todoList.value = json) )
+// const isLoad = ref(false);
+
+//aaaa; ekrana error u bastırır.
+await fetch ("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((json) => {
+    todoList.value = json;
+    // isLoad.value = true;
+  });
 </script>
 
 <template>
-    <h3>Todos</h3>
+  <h3>Todos</h3>
   <ul>
     <li v-for="todo in todoList" :key="todo.id">
       {{ todo.title }}
     </li>
   </ul>
+  <!-- <span v-else>Loading..</span> -->
 </template>
