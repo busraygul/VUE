19649390216936e07f838abd4599c277f19180d2
busraygul <aygul.busra58@gmail.com>
2025-09-23 11:36:37 +0300

    namespaced-components

--- DERS-11/namespaced-components/src/App.vue
+++ DERS-11/namespaced-components/src/App.vue
@@ -1,30 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  // import inputPage from './components/Forms/inputPage.vue';
+  // import labelPage from './components/Forms/labelPage.vue';
+  // import selectPage from './components/Forms/selectPage.vue';
+
+  import * as Form from "./components/form-components";
+  //tek bir importla hallettik. js dosyasından export ettik.
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <!-- <labelPage>
+    Başlık
+    <inputPage />
+  </labelPage>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <labelPage>
+    Kategori
+    <selectPage />
+  </labelPage> -->
+
+  <Form.labelPage>
+    Başlık
+    <Form.inputPage />
+  </Form.labelPage>
+
+  <Form.labelPage>
+    Kategori
+    <Form.selectPage />
+  </Form.labelPage>
+</template>
---
+++ DERS-11/namespaced-components/src/components/form-components.js
@@ -0,0 +1,3 @@
+export { default as inputPage } from "./Forms/inputPage.vue"
+export { default as selectPage } from "./Forms/selectPage.vue"
+export { default as labelPage } from "./Forms/labelPage.vue"
